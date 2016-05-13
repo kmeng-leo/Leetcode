aae1c989f8b2ed1dfd280e4624962547dfcf7d01
kmeng1989 <kmeng1989@git.com>
2016-05-13 17:12:37 +0800

    Reverse String

--- Leetcode/Java/Reverse_String/SolutionTest.java
+++ Leetcode/Java/Reverse_String/SolutionTest.java
@@ -1,14 +1,24 @@
 package Reverse_String;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testReverseString() {
+		String str = "Hello";
+		Solution solution = new Solution();
+		String str2 = solution.reverseString(str);
+		Assert.assertEquals("olleH", str2);
+	}
+
+	@Test
+	public void testReverseString1() {
+		String string = "";
+		Solution solution = new Solution();
+		String str2 = solution.reverseString(string);
+		Assert.assertEquals("", str2);
 	}
 
 }
