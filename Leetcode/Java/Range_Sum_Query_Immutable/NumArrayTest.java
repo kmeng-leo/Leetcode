7257534e2055840e0c312eb4db24948b811e3244
kmeng1989 <kmeng1989@git.com>
2016-05-13 16:48:25 +0800

    Range Sum Query - Immutable

---
+++ Leetcode/Java/Range_Sum_Query_Immutable/NumArrayTest.java
@@ -0,0 +1,79 @@
+package Range_Sum_Query_Immutable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumArrayTest {
+
+	@Test
+	public void testNumArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSumRange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
