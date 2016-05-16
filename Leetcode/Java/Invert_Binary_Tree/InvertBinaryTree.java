23477640f0b2f1db0209ea7844572365194765e2
kmeng1989 <kmeng1989@git.com>
2016-05-16 10:18:14 +0800

    Invert Binary Tree

---
+++ Leetcode/Java/Invert_Binary_Tree/InvertBinaryTree.java
@@ -0,0 +1,24 @@
+package Invert_Binary_Tree;
+
+public class InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode tempNode = root.left;
+		root.left = root.right;
+		root.right = tempNode;
+		invertTree(root.left);
+		invertTree(root.right);
+		return root;
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	public TreeNode(int v) {
+		val = v;
+	}
+}
